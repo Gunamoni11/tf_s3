174319fc3ca47bf8763295944ea68c461fd6bf13
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-23 15:56:19 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,9 +2,9 @@ provider "aws" {
 region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "b1" {
   bucket = "my-tf-test-bucket"
-  acl    = "private"
+  acl    = "public"
 
   tags = {
     Name        = "My bucket"

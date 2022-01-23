323b3719ddb456be720fff78be2ea6d65c8c2f81
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-23 15:54:07 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,17 @@
 provider "aws" {
 region = "us-east-2"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 resource "aws_s3_bucket" "b" {
   bucket = "s3-website-test.hashicorp.com"
   acl    = "public-read"
